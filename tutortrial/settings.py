412d7d701c5d3e43852299dcec62bad9f3b202de
Admin_wzt <wuzhentang@debian7.wzt>
2014-12-25 11:14:54 +0800

    first commit a easy scrapy demo

---
+++ tutortrial/settings.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+# Scrapy settings for tutortrial project
+#
+# For simplicity, this file contains only the most important settings by
+# default. All the other settings are documented here:
+#
+#     http://doc.scrapy.org/en/latest/topics/settings.html
+#
+
+BOT_NAME = 'tutortrial'
+
+SPIDER_MODULES = ['tutortrial.spiders']
+NEWSPIDER_MODULE = 'tutortrial.spiders'
+
+# Crawl responsibly by identifying yourself (and your website) on the user-agent
+#USER_AGENT = 'tutortrial (+http://www.yourdomain.com)'
---
+++ tutortrial/spiders/dmoz_spider.py
@@ -0,0 +1,15 @@
+#! /usr/bin/env python
+
+from scrapy.spider import Spider
+
+class DmozSpider(Spider):
+	name="dmoz"
+	allowed_domains=["dmoz.org"]
+	start_urls = [
+		"http://www.dmoz.org/Computers/Programming/Languages/Python/Books/",  
+		        "http://www.dmoz.org/Computers/Programming/Languages/Python/Resources/ "
+	]
+	def parse(self,response):
+		filename=response.url.split("/")[-2]
+		open(filename,'wb').write(response.body)
+
